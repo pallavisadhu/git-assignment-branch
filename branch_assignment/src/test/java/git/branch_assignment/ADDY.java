c3a861834c617d5cc24afb8ac975f28657c9c50c
pallavisadhu <pallavisadhu@yahoo.com>
2022-12-09 15:43:51 -0600

    added addY java file

---
+++ branch_assignment/src/test/java/git/branch_assignment/ADDY.java
@@ -0,0 +1,61 @@
+package git.branch_assignment;
+
+import java.util.ArrayList;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ADDY {
+	
+	public ArrayList<String> addY(ArrayList<String> al) {
+		
+		ArrayList<String> output = new ArrayList<String>();
+		for(String s : al) {
+			output.add("y"+s+"y");
+		
+		}
+		System.out.println(output);
+		return output;
+	}
+	
+	
+	
+	@Test
+	public void testCase1() {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("a");
+		al.add("b");
+		al.add("c");
+		ArrayList<String> actual = addY(al);
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("yay");
+		expected.add("yby");
+		expected.add("ycy");
+		Assert.assertEquals(actual, expected);
+
+	}
+	
+	@Test
+	public void testCase2() {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("hello");
+		al.add("there");
+		ArrayList<String> actual = addY(al);
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("yhelloy");
+		expected.add("ytherey");
+		Assert.assertEquals(actual, expected);
+
+	}
+	@Test
+	public void testCase3() {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("yay");
+		ArrayList<String> actual = addY(al);
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("yyayy");
+		Assert.assertEquals(actual, expected);
+
+	}
+
+}
